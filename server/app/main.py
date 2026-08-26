7b629eab31c51fc84e2647378dc80d6dbea6cda1
Marut Shah <shahmarut01@gmail.com>
2026-08-26 02:10:27 -0400

    Prepare backend for deployment

--- server/app/main.py
+++ server/app/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.database import Base, engine
+from app.models import HealthMetric, User
 from app.routers import (
     auth_router,
+    dashboard_router,
     health_metrics_router,
     users_router,
-    dashboard_router,
 )
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="BioTrack AI API")
 
@@ -22,6 +26,7 @@ app.include_router(users_router)
 app.include_router(auth_router)
 app.include_router(health_metrics_router)
 app.include_router(dashboard_router)
+
 
 @app.get("/")
 def root():
